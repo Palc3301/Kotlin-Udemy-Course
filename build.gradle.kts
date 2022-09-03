159e8d51c10883b51e3c120c8ef19d758247563e
Pedro Arruda <pedroarruda8@live.com>
2022-09-02 21:25:10 -0300

    Testes Unitarios

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 }
 
 dependencies {
+    implementation("junit:junit:4.13.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation(kotlin("test"))
 }
 
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.*
+import secao10.abc
+import secao10.countXO
+
+class MainTest {
+
+    @Test
+    @DisplayName("Teste método xxoo")
+    fun testCountXO() {
+        //todos metodos serem executados independente se um falhar
+        Assertions.assertAll(
+            //{Assertions.assertTrue(countXO("xxooxx"))},
+            {Assertions.assertTrue(countXO("xxoo"))},
+           //{Assertions.assertTrue(countXO("xxOo"))},
+            //{Assertions.assertTrue(countXO("Xxoo"))},
+        )
+    }
+    @Test
+    @Disabled
+    fun naoImplementado() {
+
+    }
+
+    @Test
+    fun vaiFalhar() {
+        fail("Não posso terminar os testes sem esse metodo")
+    }
+
+    @Test
+    fun assumption() {
+     //   Assumptions.assumeTrue(countXO("xxoo"))
+       // Assertions.assertTrue(abc())
+    }
+
+    @Test
+    fun exception() {
+        assertThrows<NullPointerException> { abc() }
+    }
+
+}

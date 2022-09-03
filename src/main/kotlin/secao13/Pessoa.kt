401f9a5f731b7cb630fedf2c45d720e994c088ca
Pedro Arruda <pedroarruda8@live.com>
2022-09-03 10:48:02 -0300

    Primary & secondary Constructors

--- src/main/kotlin/secao13/Pessoa.kt
+++ src/main/kotlin/secao13/Pessoa.kt
@@ -1,8 +1,15 @@
 package secao13
 
 class Pessoa(val ano: Int, var nome: String) {
-
     var olhos: String = ""
+
+    //pode ser null
+    var doc: String? = null
+    //this instancia da classe-se refere ao objeto
+    constructor(ano: Int, nome: String, doc:String) : this(ano,nome){
+        this.doc = doc
+        this.nome = nome
+    }
     fun dormir() {
 
     }
@@ -11,11 +18,21 @@ class Pessoa(val ano: Int, var nome: String) {
     }
 }
 
-fun main() {
-    var pessoa: Pessoa = Pessoa(2002, "Pedro")
+class Vazia
+//classe não pode ser instanciada
+//class Vazia private constructor()
 
+
+fun main() {
+
+    var pessoa: Pessoa = Pessoa(2002, "Pedro", "3301")
+
+    //construtor
+    Vazia()
+    //-----------------
     pessoa.nome
     pessoa.acordar()
     pessoa.dormir()
     pessoa.olhos = "Castanhos"
+    pessoa.doc
 }
